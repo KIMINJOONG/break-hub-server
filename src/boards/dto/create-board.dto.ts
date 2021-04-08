1b0305ba59afad6073b2808ccd41b5370be1a07d
KIMINJOONG <ijboym216@gmail.com>
2021-04-08 17:42:50 +0900

    init

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,18 +1,25 @@
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { IsArray, IsNumber, IsString } from 'class-validator';
 
+@ApiTags('게시글 생성 요청 데이터')
 export class CreateBoardDto {
   @IsString()
+  @ApiProperty({ type: String, description: '제목' })
   readonly title: string;
 
   @IsString()
+  @ApiProperty({ type: String, description: '내용' })
   readonly content: string;
 
   @IsString()
+  @ApiProperty({ type: String, description: '비디오 링크' })
   readonly videoUrl: string;
 
   @IsArray()
+  @ApiProperty({ type: Array, description: '비디오 링크' })
   readonly searchTagSeqs: number[];
 
   @IsNumber()
+  @ApiProperty({ type: Number, description: '비디오 링크' })
   readonly categorySeq: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3000', credentials: true },
+  });
+  // app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API DOCS EXAMPLE!')
+    .setDescription('API 문서입니다.')
+    .setVersion('0.0.1')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
